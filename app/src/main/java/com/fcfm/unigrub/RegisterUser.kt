0c31a719be653498c8f9420e997af78d612cfde1
Martin Garza <54215833+MartinGarzaFCFM@users.noreply.github.com>
2023-04-01 13:48:07 -0600

    Login y Register Activity Agregados

---
+++ app/src/main/java/com/fcfm/unigrub/RegisterUser.kt
@@ -0,0 +1,11 @@
+package com.fcfm.unigrub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registeruser)
+    }
+}
